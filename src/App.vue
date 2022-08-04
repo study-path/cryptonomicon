10926fa4f75aafa2ab20bc9781fa59943f29e836
study-path <liliyaingul@gmail.com>
2022-08-04 12:03:38 -0400

    add Tailwind CSS

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,176 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="ticker">
+      <label class="bg-orange-900">Ticker</label>
+      <input
+        type="text"
+        v-model="ticker"
+        @keydown.enter="add"
+        name="ticker"
+        id="ticker"
+      />
+      <div class="bg-slate-200" if="checkTicker(ticker)">{{ ticker }}</div>
+      <label for="">Ticker already added</label>
+      <button @click="add">Add</button>
+    </div>
+    <template v-if="tickers.length">
+      <hr />
+
+      <div class="container">
+        <div
+          class="flex flex-col justify-items-center"
+          :class="{
+            'green-border': sel === t,
+          }"
+          v-for="(t, index) in tickers"
+          :key="index"
+          @click="select(t)"
+        >
+          <div>{{ t.name }} - USD</div>
+          <div>{{ t.price }}</div>
+
+          <button @click.stop="deleteTicker(t)">Delete</button>
+        </div>
+      </div>
+      <hr />
+    </template>
+    <section v-if="sel" class="graph">
+      <h3 class="graph">{{ sel.name }} - USD</h3>
+      <div class="axisX axisY graph">
+        <div
+          class="bar"
+          v-for="(bar, index) in normalizeGraph()"
+          :key="index"
+          :style="{ height: `${bar}%` }"
+        ></div>
+      </div>
+      <button @click="sel = null" type="button">Close</button>
+    </section>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      ticker: "",
+      tickers: [],
+      sel: null,
+      graph: [],
+    };
+  },
+  methods: {
+    add() {
+      const currentTicker = {
+        name: this.ticker,
+        price: "-",
+      };
+
+      this.tickers.push(currentTicker);
+
+      setInterval(async () => {
+        const f = await fetch(
+          `https://min-api.cryptocompare.com/data/price?fsym=${currentTicker.name}&tsyms=USD&api_key=42900433546e3911f97fcb744153bb343102a612c5cf36f1a948b460f9f1c8d2`
+        );
+        const data = await f.json();
+        this.tickers.find((t) => t.name === currentTicker.name).price =
+          data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2);
+
+        if (this.sel?.name === currentTicker.name) {
+          this.graph.push(data.USD);
+        }
+      }, 3000);
+
+      this.ticker = "";
+    },
+
+    select(ticker) {
+      this.sel = ticker;
+      this.graph = [];
+    },
+
+    deleteTicker(tickerToRemove) {
+      this.tickers = this.tickers.filter((t) => t != tickerToRemove);
+    },
+    normalizeGraph() {
+      const maxValue = Math.max(...this.graph);
+      const minValue = Math.min(...this.graph);
+      return this.graph.map(
+        (price) => 5 + ((price - minValue) * 95) / (maxValue - minValue)
+      );
+    },
+    checkTicker(name) {
+      const res = this.tickers.filter((ticker) => ticker != name);
+      return res;
+    },
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.ticker {
+  padding: 10px;
+}
+
+label {
+  padding: 5px;
+}
+
+input {
+  margin: 5px;
+}
+button {
+  margin: 10px 0;
+}
+
+.container {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+
+.inner {
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  align-content: space-around;
+  align-items: center;
+  border: 1px solid grey;
+  padding: 10px;
+  width: 150px;
+  height: 100px;
+}
+dl {
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+  gap: 10px;
+}
+.green-border {
+  border: 3px solid green;
+  padding: 5px;
+}
+.bar {
+  background-color: rgb(149, 235, 235);
+  border: 1px solid rgb(75, 190, 224);
+  padding: 0 10px;
+}
+/* .box {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+} */
+
+.axisX {
+  border-bottom: 1px groove gray;
+  width: 300px;
+}
+.axisY {
+  display: flex;
+  align-items: flex-end;
+  border-left: 1px groove gray;
+  height: 300px;
 }
 </style>
